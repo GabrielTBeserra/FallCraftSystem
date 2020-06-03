437f0adde175021d55b28c0cdd29d0caee5cfbb2
PedroDiniz123 <pedrojunqueira5@gmail.com>
2020-06-03 00:55:59 -0300

    Update Chest
    Sem erro na troca de rankings (Vips)

--- src/main/java/fallcraftsystem/modules/chest/events/InventoryClose.java
+++ src/main/java/fallcraftsystem/modules/chest/events/InventoryClose.java
@@ -3,11 +3,16 @@ package fallcraftsystem.modules.chest.events;
 import fallcraftsystem.core.FallCraftSystem;
 import fallcraftsystem.modules.chest.utils.ChestsList;
 import fallcraftsystem.modules.chest.utils.ToBase64;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
 
 public class InventoryClose implements Listener {
     FallCraftSystem plugin;
@@ -28,9 +33,24 @@ public class InventoryClose implements Listener {
         if (event.getInventory().getName().contains("§fFall§bCraft §9Baú")) {
             player = (Player) event.getInventory().getHolder();
 
-            ItemStack[] contents = event.getInventory().getContents();
-            String base64 = ToBase64.itemStackArrayToBase64(contents);
+            int size = event.getInventory().getSize();
+            boolean empty = true;
 
+            for (int i = 0; i < size; i++) {
+                if (event.getInventory().getItem(i) != null) {
+                    empty = false;
+                    break;
+                }
+            }
+
+            ItemStack[] contents = event.getInventory().getContents();
+
+            if (empty) {
+                Inventory inv = Bukkit.createInventory(player, 9);
+                contents = inv.getContents();
+            }
+
+            String base64 = ToBase64.itemStackArrayToBase64(contents);
             if (!ChestsList.getChestListFile().contains("chests.players." + player.getName() + ".chest_items")) {
                 ChestsList.getChestListFile().createSection("chests.players." + player.getName() + ".chest_items");
             }
